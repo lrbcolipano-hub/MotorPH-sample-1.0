31c5c669fce0d854d6a89e5620c8f57c533d6c43
lrbcolipano-hub <lr.bcolipano@mmdc.mcl.edu.ph>
2026-02-22 05:30:47 +0800

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,42 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.motorph;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-
-/**
- *
- * @author Anya
- */
 public class Employee {
     //attributes of each employees (employee name, birthdate and id
-    String name;
-    LocalDate birthDate;
-    int id;
-     
-    //constructor for the attributes of name, birthdate and id
-    Employee(String name, LocalDate birthDate, int id) {
-        this.name = name;
-        this.birthDate = birthDate;
-        this.id = id;
+    String employeeId;
+    String lastName;
+    String firstName;
+    String birthDate;
+    
+    //constructor for the Employee class
+    public Employee (String employeeId, String lastName, String firstName, String birthDate){
+        this.employeeId = employeeId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate  = birthDate;  
     }
-
-    //lets set a displayBirthdate method to display birthdate
-    public String displayBirthDate (){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");//add DateTimeFormatter class to display the birth date in a more readable format
-        String formattedDate = birthDate.format(formatter);
-        return formattedDate;    
-    }
-    //lets set a display name method to display name
-    public String displayName () {
-        return name;
-    }
-    //lets set a display ID method to display ID
-    private int displayID () {
-        return id;
-    }
+    public static List<Employee> motorPhCsv(String path){
+        List<Employee> employees = new ArrayList<> ();
+        String line;
+        
+        try {
+            //use BufferedReader class to read the MotorPH CSV file
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            
+            //while loop to run to the file infinitely as long as the CSV file is  read
+            while ((line = br.readLine()) !=null) {
+                //split by comma
+                String[] values = line.split(",");
+                
+                //Assign values to the attributes of each Object
+                Employee employee = new Employee(values[0],values[1],values[2],values[3]);
+                employees.add(employee);
+            }
+            
+            
+        } catch (FileNotFoundException ex) {
+            System.getLogger(MotorPhData.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }catch (IOException ex) {
+            System.getLogger(MotorPhData.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    
+        return employees;
 }
+}
+
--- MotorPH.java
+++ MotorPH.java
@@ -7,31 +7,26 @@ package com.mycompany.motorph;
  *
  * @author Anya
  */
-import java.time.LocalDate;
-import java.util.ArrayList;
+import static com.mycompany.motorph.Employee.motorPhCsv;
 import java.util.List;
 
 public class MotorPH {
 
     public static void main(String[] args) {
         //Arraylist to store the Employees objects
-        List<Employee> employees = new ArrayList<>();
-        
-        //Add employees on the array with their respective parameters
-        employees.add(new Employee("Garcia, Manuel III",LocalDate.of(1983, 10, 11),10001));
-        employees.add(new Employee("Lim, Antonio",LocalDate.of(1988, 6, 19),10002));
-        employees.add(new Employee("Aquino, Bianca Sofia", LocalDate.of(1989, 8, 4),10003));
-        employees.add(new Employee("Reyes, Isabella", LocalDate.of(1994, 6, 16),10004));
-        employees.add(new Employee("Hernandez, Eduard", LocalDate.of(1989, 9, 23),10004));
-        
-        
-        //for loop to iterate through each objects for details printing 
+        String path = "C:\\Users\\Anya\\Downloads\\Copy of MotorPH_Employee Data - Employee Details.csv";
+
+        List<Employee> employees = motorPhCsv(path);
+       
         for (Employee employee : employees) {
-            System.out.println("Employee #: " + employee.id);
-            System.out.println("Employee Name: "+ employee.displayName());
-            System.out.println("Birthday: " + employee.displayBirthDate());
-            System.out.println(" ");
+            System.out.println("Employee #: " + employee.employeeId);
+            System.out.println("Employee Name: " + employee.lastName + ", " +employee.firstName);
+            System.out.println("BirthDate: " + employee.birthDate);
+            System.out.println("");
         }
+        
+       
+        
     }
     
 }
