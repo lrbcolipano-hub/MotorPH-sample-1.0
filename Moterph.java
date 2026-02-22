064674796169f66cf79a5cd06069f1019aea08b4
lrrhonrado13 <lr.rhonrado@mmdc.mcl.edu.ph>
2026-02-22 10:36:48 +0800

    Add files via upload

--- Moterph.java
+++ Moterph.java
@@ -4,6 +4,8 @@
  */
 package moterph;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rachelda Honrado
@@ -14,6 +16,37 @@ public class Moterph {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        //===================== LOGIN SYSTEM ======================================
+        String CorrectUsername = "admin";
+        String correctPassword = "1234";
+        boolean loggedIn = false;
+        
+        for (int attempt = 1; attempt <= 3; attempt++){ 
+            System.out.println("==========MOTORPH LOGIN========");
+            System.out.println("Username: ");
+            String username = input.nextLine();
+            
+            System.out.print("Password: ");
+            String password = input.nextLine();
+           
+            if (password.equals(correctPassword)&& username.equals(CorrectUsername)){
+                loggedIn = true;
+                System.out.println("\nLogin Successfull!\n");
+                break;
+            }else{
+                System.out.println("Invalid credentials.Attempt"+ attempt + "of3.\n");
+            }
+        }
+        if (!loggedIn){
+            System.out.println("Too many failed attempts.System locked.");
+            return;
+        }
+        
+        
+        //====================== EMPLOYEE DATABASE ==========================
         String[][] employees = {
             {"10001","Garcia, Manuel III", "1983-Oct-11","90000","1500","2000","1000"},
             {"10002","Lim, Antonio","1988-Jun-19","60000","1500","2000","1000"},
@@ -21,39 +54,57 @@ public class Moterph {
             {"10004","Reyes, Isabella","1994-Jun-16","52670","1500","1000","1000"},
             {"10005", "Hernadez, Eduard","1989-Sep-23","52670","1500","1000","1000"},
         };
-        
-        for (String[] employee : employees) {
-            String empNumber = employee[0];
-            String empName = employee[1];
-            String birthday = employee[2];
-            double basicSalary = Double.parseDouble(employee[3]);
-            double rice = Double.parseDouble(employees[1][4]);
-            double phone = Double.parseDouble(employee[5]);
-            double clothing = Double.parseDouble(employee[6]);
-            double grossSalary = basicSalary + rice + phone + clothing;
-            double sss = computeSSS(grossSalary);
-            double philhealth = (basicSalary*0.03)/2;
-            double pagibig = 100.00;
-            double totalDeductions = sss + philhealth + pagibig;
-            double taxableIncome = grossSalary - totalDeductions;
-            double withholdingTax = computeTax(taxableIncome);
-            double netSalary = grossSalary - totalDeductions - withholdingTax;
-           
-            //==================OUTPUT=================
-            System.out.println("Employee#:" + empNumber);
-            System.out.println("Employee Name:" + empName);
-            System.out.println("Birthday:" + birthday);
-            System.out.println("-------------------------------------------");
-            System.out.println("Gross Salary:" + grossSalary);
-            System.out.println("SSS Deduction:" + sss);
-            System.out.println("PhilHealth:" + philhealth);
-            System.out.println("Pag-IBIG:" + pagibig);
-            System.out.println("Withholding Tax:" + withholdingTax);
-            System.out.println("Net Salary   :" + netSalary);
-            System.out.println("=========================================\n");
-        }
-    }
-    
+        //======================= SEARCH LOOP ==================================
+        while (true){
+            System.out.print("Enter Employee Number (or type `exit`):");
+            String empSearch = input.nextLine();
+            
+            if (empSearch.equalsIgnoreCase("Exit")){
+                System.out.println("System Closed.");
+                break;
+            }
+            
+            boolean found = false;
+            for (String[] employee : employees) {
+                if (employee[0].equals(empSearch)) {
+                    found = true;
+                    String empNumber = employee[0];
+                    String empName = employee[1];
+                    String birthday = employee[2];
+                    double basicSalary = Double.parseDouble(employees[1][3]);
+                    double rice = Double.parseDouble(employee[4]);
+                    double phone = Double.parseDouble(employee[5]);
+                    double clothing = Double.parseDouble(employee[6]);
+                    double grossSalary = basicSalary + rice + phone + clothing;
+                    double sss = computeSSS(grossSalary);
+                    double philhealth = (basicSalary*0.03)/2;
+                    double pagibig = 100.00;
+                    double totalDeductions = sss + philhealth + pagibig;
+                    double taxableIncome = grossSalary - totalDeductions;
+                    double withholdingTax = computeTax(taxableIncome);
+                    double netSalary = grossSalary - totalDeductions - withholdingTax;
+                    //==================OUTPUT=================
+                    System.out.println("/n===========EMPLOYEE PAYROLL==========");
+                    System.out.println("Employee#:" + empNumber);
+                    System.out.println("Employee Name:" + empName);
+                    System.out.println("Birthday:" + birthday);
+                    System.out.println("-------------------------------------------");
+                    System.out.println("Gross Salary:" + grossSalary);
+                    System.out.println("SSS Deduction:" + sss);
+                    System.out.println("PhilHealth:" + philhealth);
+                    System.out.println("Pag-IBIG:" + pagibig);
+                    System.out.println("Withholding Tax:" + withholdingTax);
+                    System.out.println("Net Salary   :" + netSalary);
+                    System.out.println("=========================================\n");
+                }
+            }
+    if (!found){
+     System.out.print("Employee not found.\n");
+}
+}
+input.close();
+
+}
     //===================== SSS COMPUTATION ============================
     public static double computeSSS (double salary){
         
